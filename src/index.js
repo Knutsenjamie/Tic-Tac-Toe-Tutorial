84859e8d7653bde365a3526a8743baa28f92e10a
“Jamie <knutsenjamie@yahoo.com>
2021-09-26 12:55:18 -0700

    add onClick and remove state from Square

--- src/index.js
+++ src/index.js
@@ -3,21 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
-
     render() {
         return (
-            <button 
+        <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
-            >
-                {this.state.value}
-            </button>
+            onClick={() => this.props.onClick()}
+        >
+            {this.props.value}
+        </button>
         );
     }
 }
@@ -33,8 +26,12 @@ class Board extends React.Component {
 
 
     renderSquare(i) {
-        return <Square value={i} />;
-    }
+        return ( <Square 
+        value={this.state.squares[i]} 
+        onClick={() => this.handleClick(i)}
+        />
+    );
+}
 
     render() {
         const status = 'Next Player: X';
