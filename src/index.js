2b8ac7caa5c9a1b64d6f6ad42087372fa46ed07e
“Jamie <knutsenjamie@yahoo.com>
2021-09-27 07:55:50 -0700

    add O's

--- src/index.js
+++ src/index.js
@@ -19,13 +19,18 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        // remember that the ? is an alternative way of writing an if statement; essentially best to be used in the case where one of two values will be assigned to a variable based on a conditional statement.
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
@@ -38,7 +43,7 @@ class Board extends React.Component {
 }
 
 render() {
-    const status = 'Next Player: X';
+    const status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
         <div>
