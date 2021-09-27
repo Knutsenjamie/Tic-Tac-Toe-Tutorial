a011945a9752f700eb1b7fb3bc5a9bd0054241c7
“Jamie <knutsenjamie@yahoo.com>
2021-09-27 07:42:55 -0700

    add handleClick function

--- src/index.js
+++ src/index.js
@@ -23,27 +23,32 @@ class Board extends React.Component {
         };
     }
 
-
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
 
     renderSquare(i) {
-        return ( <Square 
-        value={this.state.squares[i]} 
-        onClick={() => this.handleClick(i)}
+        return ( 
+        <Square 
+            value={this.state.squares[i]} 
+            onClick={() => this.handleClick(i)}
         />
     );
 }
 
-    render() {
-        const status = 'Next Player: X';
+render() {
+    const status = 'Next Player: X';
 
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
+    return (
+        <div>
+            <div className="status">{status}</div>
+            <div className="board-row">
+                {this.renderSquare(0)}
+                {this.renderSquare(1)}
+                {this.renderSquare(2)}
+            </div>
             <div className="board-row">
                 {this.renderSquare(3)}
                 {this.renderSquare(4)}
